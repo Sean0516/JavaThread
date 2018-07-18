a786daa086f12868d9e7854ffcbf7341a5aebbe4
sean <584333869@qq.com>
2018-07-18 13:56:20 +0800

    executors  工具类

---
+++ Thread/src/com/voicecyber/executors/TestExecutors.java
@@ -0,0 +1,19 @@
+package com.voicecyber.executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Sean on 2018/7/18
+ *
+ * @author Sean
+ */
+public class TestExecutors {
+    public static void main(String[] args) {
+        ExecutorService executorService= Executors.newFixedThreadPool(10);
+        executorService=Executors.newCachedThreadPool();
+        executorService=Executors.newSingleThreadExecutor();
+        executorService=Executors.newScheduledThreadPool(10);
+    }
+
+}
